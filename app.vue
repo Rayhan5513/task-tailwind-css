85c22e8a33108bd149f4883a0375e2e126a7fe8d
Rayhan5513 <58135041+Rayhan5513@users.noreply.github.com>
2025-01-06 17:35:33 +0600

    Task tailwindCSS

---
+++ app.vue
@@ -0,0 +1,154 @@
+<template>
+
+
+
+
+<header class="bg-white text-center p-4">
+  
+   <img src="/assets/images/headImage.png" class="w-24 h-auto mx-auto">
+   
+</header>
+
+<div class="text-center">
+    <h2 class="text-green-800">স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!</h2>
+    <h2 class="text-green-800">"আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</h2>
+    <h3 class="mt-4 px-4 bg-yellow-400 inline-block">আপনার জীবনকে আরও স্মার্ট করুন।</h3>
+</div>
+
+<div class="block mt-8 mx-auto relative  md:w-3/5 w-max-[85%] w-[55%] aspect-[16/9]">
+  <iframe
+  class="absolute w-full h-full "
+    src="https://www.youtube.com/embed/hZ1DASYd9rk?si=j16WdsuKXngwwrCe"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    referrerpolicy="strict-origin-when-cross-origin"
+    allowfullscreen
+    
+  ></iframe>
+</div>
+
+
+
+
+<div class="flex justify-center items-center h-24 mt-12 pb-8">
+    <button class="bg-green-800 text-yellow-400 px-8 py-4 flex items-center justify-center text-lg font-medium rounded-xl transition-colors duration-300 hover:bg-blue-900">
+        অর্ডার করতে ক্লিক করুন
+        
+        <img src="/assets/images/clickButtonSign.png" class="w-5 h-auto ml-2">
+        
+  
+    </button>
+</div>
+
+<div class="bg-yellow-100 py-16">
+    <h1 class="ml-[20%]  text-2xl">স্মার্টওয়াচের একাধিক ফিচার<br>এক নজরে</h1>
+
+    <div class="grid grid-cols-3 gap-4 mx-1/6 p-5 rounded-lg">
+        <div class=" group border-2 border-green-800 p-5 rounded-tl-2xl rounded-br-2xl hover:bg-green-800">
+            <h3 class="text-green-800 group-hover:text-yellow-400">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
+            <p class="text-gray-700 group-hover:text-white">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+        </div>
+        <div class="group border-2 border-green-800 p-5 rounded-tl-2xl rounded-br-2xl hover:bg-green-800">
+            <h3 class="text-green-800 group-hover:text-yellow-400">স্বাস্থ্য পর্যবেক্ষণ সুবিধা</h3>
+            <p class="text-gray-700 group-hover:text-white">হার্ট রেট মনিটর: দিনজুড়ে আপনার হার্টের অবস্থার নজর রাখুন।
+                রক্তের অক্সিজেন পর্যবেক্ষণ: SpO2 ট্র্যাকিং দিয়ে স্বাস্থ্য সম্পর্কে সচেতন থাকুন।
+                স্লিপ ট্র্যাকিং: গভীর ও হালকা ঘুমের স্তর বিশ্লেষণ করে সঠিক পরামর্শ।</p>
+        </div>
+        <div class="group border-2 border-green-800 p-5 rounded-tl-2xl rounded-br-2xl hover:bg-green-800">
+            <h3 class="text-green-800 group-hover:text-yellow-400">ফিটনেস ট্র্যাকিং</h3>
+            <p class="text-gray-700 group-hover:text-white">স্পোর্টস মোড: বিভিন্ন ধরনের স্পোর্টস অ্যাক্টিভিটির জন্য ১০+ মোড।
+                ক্যালোরি বার্ন কাউন্টার: আপনার ব্যায়ামের সময় ক্যালোরি খরচ নির্ধারণ করুন।
+                স্টেপ ট্র্যাকার: প্রতিদিনের পদক্ষেপ হিসেব করুন।</p>
+        </div>
+        <div class="group border-2 border-green-800 p-5 rounded-tl-2xl rounded-br-2xl hover:bg-green-800">
+            <h3 class="text-green-800 group-hover:text-yellow-400">স্মার্ট নোটিফিকেশন</h3>
+            <p class="text-gray-700 group-hover:text-white">কল, মেসেজ, এবং অ্যাপ নোটিফিকেশন সরাসরি আপনার হাতে। গুরুত্বপূর্ণ আপডেট কখনোই মিস করবেন না।</p>
+        </div>
+        <div class="group border-2 border-green-800 p-5 rounded-tl-2xl rounded-br-2xl hover:bg-green-800">
+            <h3 class="text-green-800 group-hover:text-yellow-400">দীর্ঘস্থায়ী ব্যাটারি লাইফ</h3>
+            <p class="text-gray-700 group-hover:text-white">একবার চার্জে ৭+ দিন ব্যবহারের সুবিধা। দ্রুত চার্জিং প্রযুক্তি।</p>
+        </div>
+
+        <div class="group border-2 border-green-800 p-5 rounded-tl-2xl rounded-br-2xl hover:bg-green-800">
+    <h3 class="text-green-800 group-hover:text-yellow-400">মিউজিক এবং ক্যামেরা নিয়ন্ত্রণ</h3>
+    <p class="text-gray-700 group-hover:text-white">মিউজিক চালানো বা ক্যামেরা ক্লিক করুন আপনার স্মার্টওয়াচ থেকে।
+        সহজ এবং সাবলীল ব্যবহারের সুবিধা।</p>
+</div>
+
+    </div>
+
+    <div class="flex justify-center items-center h-24 mt-12 pb-8">
+        <button class="bg-green-800 text-yellow-400 px-8 py-4 flex items-center justify-center text-lg font-medium rounded-xl transition-colors duration-300 hover:bg-blue-900">
+            অর্ডার করতে ক্লিক করুন
+            
+            <img src="/assets/images/clickButtonSign.png" class="w-5 h-auto ml-2">
+            
+        </button>
+    </div>
+</div>
+
+<div class="py-16">
+    <h1 class="ml-[20%] text-2xl mb-5">আমাদের পণ্য নিয়ে গ্রাহকদের অভিজ্ঞতা</h1>
+    <div class="grid grid-cols-2 gap-4 mx-10">
+        <div class="bg-green-800 text-white p-8 rounded-lg">
+            <p>সন্তুষ্ট গ্রাহক</p>
+            <h1 class="text-yellow-400 text-2xl text-left ">৩,০০০+</h1>
+            <h5>সন্তুষ্ট গ্রাহকরা আমাদের পণ্যের প্রতি বিশ্বাস রেখেছেন।</h5>
+        </div>
+        <div class="bg-green-800 text-white p-8 rounded-lg">
+            <p>পুনঃক্রয়কারী</p>
+            <h1 class="text-yellow-400">৪০০+</h1>
+            <h5>পুনঃক্রয় করে আমাদের গুণমান নিশ্চিত করে।</h5>
+        </div>
+        <div class="bg-green-800 text-white p-8 rounded-lg">
+            <p>পণ্য বিক্রি</p>
+            <h1 class="text-yellow-400">৫০০০+</h1>
+            <h5>৫০০০ পণ্যের বিক্রি আমাদের জনপ্রিয়তা প্রমাণ করে।</h5>
+        </div>
+        <div class="bg-green-800 text-white p-8 rounded-lg">
+            <p>গ্রাহক সন্তুষ্ট</p>
+            <h1 class="text-yellow-400">৯৫%</h1>
+            <h5>৯৫% গ্রাহক আমাদের সেবা ও পণ্যে সন্তুষ্ট।</h5>
+        </div>
+    </div>
+
+    <div class="flex justify-center items-center h-24 mt-12 pb-8">
+        <button class="bg-green-800 text-yellow-400 px-8 py-4 flex items-center justify-center text-lg font-medium rounded-xl transition-colors duration-300 hover:bg-blue-900">
+            অর্ডার করতে ক্লিক করুন
+            
+            <img src="/assets/images/clickButtonSign.png" class="w-5 h-auto ml-2">
+            
+        </button>
+    </div>
+</div>
+
+<footer class="bg-green-900 text-white py-8">
+    <div class="max-w-screen-xl mx-auto flex justify-between items-center px-5">
+        <div class="flex items-center space-x-4">
+          
+            <img src="/assets/images/callImg.png" alt="Call Icon">
+          
+            <p class="text-left">+880 123 456 790</p>
+        </div>
+        
+        <div>
+            <img src="/assets/images/footerImg.png" alt="Footer Image" class="mx-auto">
+        </div>
+        
+        <div>
+            <img src="/assets/images/footerMedia.png" alt="Facebook Icon" class="mx-auto">
+        </div>
+        
+    </div>
+    <div class="text-center bg-green-800 py-4 mt-4">
+        "© 2024 All Rights Reserved by BDFunnelBuilder. Unauthorized use prohibited."
+    </div>
+</footer>
+
+
+
+  
+</template>
